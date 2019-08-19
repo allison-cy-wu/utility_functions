4d4ecfedc5ccf30b6d8b0d4ec9d7201b590dae4a
allisonCYWu <allison.wu@thermofisher.com>
2019-08-18 21:37:22 -0700

    Bug fixes

--- utility_functions/benchmark.py
+++ utility_functions/benchmark.py
@@ -11,12 +11,12 @@ def timer(func):
         end_time = time.time()
         time_elapsed = str(datetime.timedelta(seconds = end_time - start_time)).split(':')
 
-        if int(time_elapsed[0]) >0:
+        if int(time_elapsed[0]) > 0:
             print(f"Total time taken in {func.__name__}: "
                   f"{time_elapsed[0]} hours "
                   f"{time_elapsed[2]} minutes "
                   f"{time_elapsed[3]} seconds.")
-        elif int(time_elapsed[1] > 0):
+        elif int(time_elapsed[1]) > 0:
             print(f"Total time taken in {func.__name__}: "
                   f"{time_elapsed[2]} minutes "
                   f"{time_elapsed[3]} seconds.")
