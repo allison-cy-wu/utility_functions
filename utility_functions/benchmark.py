a698061f82969d1bf266a24c11f78c873c63caad
allisonCYWu <allison.wu@thermofisher.com>
2019-08-18 18:26:03 -0700

    Add in better elapsed time format

--- utility_functions/benchmark.py
+++ utility_functions/benchmark.py
@@ -1,5 +1,6 @@
 import time
 import functools
+import datetime
 
 
 def timer(func):
@@ -8,7 +9,21 @@ def timer(func):
         start_time = time.time()
         return_value = func(*args, **kwargs)
         end_time = time.time()
-        print(f"Total time taken in {func.__name__}: {end_time - start_time} seconds.")
+        time_elapsed = str(datetime.timedelta(seconds = end_time - start_time)).split(':')
+
+        if int(time_elapsed[0]) >0:
+            print(f"Total time taken in {func.__name__}: "
+                  f"{time_elapsed[0]} hours "
+                  f"{time_elapsed[2]} minutes "
+                  f"{time_elapsed[3]} seconds.")
+        elif int(time_elapsed[1] > 0):
+            print(f"Total time taken in {func.__name__}: "
+                  f"{time_elapsed[2]} minutes "
+                  f"{time_elapsed[3]} seconds.")
+        else:
+            print(f"Total time taken in {func.__name__}: "
+                  f"{time_elapsed[3]} seconds.")
+
         return return_value
 
     return wrapper
